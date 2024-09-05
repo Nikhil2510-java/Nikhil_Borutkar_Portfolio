5e416095df99a7eaf65af877092c86e5af244a7d
Nikhil2510-java <nikhilborutkar@gmail.com>
2024-09-05 12:21:05 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*==================Typing===============================*/
+var typed=new Typed(".typing",{
+    strings:["Java Full-Stack Developer","Web Developer","Backend Developer","Java Developer"],
+    typeSpeed:100,
+    BcakSpeed:60,
+    loop:true
+})
